dcecbb1baf23e0ff323b3d4b329fbaf5207c8fd6
Stanislav Rakitov <prosperousrf@gmail.com>
2021-06-24 11:04:27 +0300

    Week2 initial

---
+++ Week2/src/SecondRatings.java
@@ -0,0 +1,17 @@
+/**
+ * Write a description of SecondRatings here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.ArrayList;
+
+public class SecondRatings {
+  private ArrayList<Movie> myMovies;
+  private ArrayList<Rater> myRaters;
+
+  public SecondRatings() {
+    // default constructor
+    this("ratedmoviesfull.csv", "ratings.csv");
+  }
+}

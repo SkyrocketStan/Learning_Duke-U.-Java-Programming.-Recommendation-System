f03a2de0a6126d51dc9bd09f5b5b79aa71a582e5
Stanislav Rakitov <prosperousrf@gmail.com>
2021-06-26 16:55:03 +0300

    Week2. SecondRatings constructor.

--- Week2/src/SecondRatings.java
+++ Week2/src/SecondRatings.java
@@ -1,17 +1,27 @@
-/**
- * Write a description of SecondRatings here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 import java.util.ArrayList;
 
+/**
+ * Week2. A new class to do many of the calculations focusing on computing averages on movie
+ * ratings.
+ *
+ * @author Stanislav Rakitov
+ * @version 1.0
+ */
 public class SecondRatings {
-  private ArrayList<Movie> myMovies;
-  private ArrayList<Rater> myRaters;
+  private final ArrayList<Movie> myMovies;
+  private final ArrayList<Rater> myRaters;
 
   public SecondRatings() {
     // default constructor
     this("ratedmoviesfull.csv", "ratings.csv");
+  }
+
+  // The constructor should create a FirstRatings object and then call the loadMovies and
+  // loadRaters methods in FirstRatings to read in all the movie and ratings data and store them
+  // in the two private ArrayList variables of the SecondRatings class, myMovies and myRaters.
+  public SecondRatings(String moviesFileName, String ratingsFileName) {
+    FirstRatings firstRatings = new FirstRatings(moviesFileName, ratingsFileName);
+    myMovies = firstRatings.getMovieArrayList();
+    myRaters = firstRatings.getRaterArrayList();
   }
 }

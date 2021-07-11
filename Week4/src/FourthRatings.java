cd3c6b942ae70c3004a318cde8bf93c71cff2a5d
Stanislav Rakitov <prosperousrf@gmail.com>
2021-07-11 19:13:54 +0300

    Week4. getAverageRatingsByFilter

--- Week4/src/FourthRatings.java
+++ Week4/src/FourthRatings.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * The week 4 class
@@ -20,5 +21,33 @@ public class FourthRatings {
           .orElse(0.0);
     }
     return 0.0;
+  }
+
+  private ArrayList<Rating> getAverageRatings(int minimalRaters) {
+    ArrayList<Rating> list = new ArrayList<>();
+    ArrayList<String> allMoviesIDs = MovieDatabase.filterBy(new TrueFilter());
+    for (String movieID : allMoviesIDs) {
+      double averageRating = getAverageByID(movieID, minimalRaters);
+      if (averageRating != 0.0) {
+        list.add(new Rating(movieID, averageRating));
+      }
+    }
+    return list;
+  }
+
+  private ArrayList<Rating> getAverageRatingsByFilter(
+      Integer minimalRaters, Filter filterCriteria) {
+    ArrayList<Rating> ratingsList = new ArrayList<>();
+    ArrayList<String> allMoviesIDs = MovieDatabase.filterBy(filterCriteria);
+    Rating rating;
+    for (String movie_id : allMoviesIDs) {
+      if (getAverageByID(movie_id, minimalRaters) != 0) {
+        rating = new Rating(movie_id, getAverageByID(movie_id, minimalRaters));
+        ratingsList.add(rating);
+      }
+    }
+
+    Collections.sort(ratingsList);
+    return ratingsList;
   }
 }

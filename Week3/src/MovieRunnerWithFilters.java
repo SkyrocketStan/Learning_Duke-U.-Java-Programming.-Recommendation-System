3f5ab184b5702bb93a6af0bcfd24bb4ae76dfcbd
Stanislav Rakitov <prosperousrf@gmail.com>
2021-07-11 15:08:02 +0300

    Week3. DirectorsFilter

--- Week3/src/MovieRunnerWithFilters.java
+++ Week3/src/MovieRunnerWithFilters.java
@@ -46,16 +46,20 @@ public class MovieRunnerWithFilters {
     printRatingsList(averageRatings);
   }
 
-  private void printRatingsList(ArrayList<Rating> averageRatings) {
-    averageRatings.stream()
+  private void printRatingsList(ArrayList<Rating> averageRatingList) {
+    System.out.printf("Found %d movies%n", averageRatingList.size());
+    averageRatingList.stream()
         .sorted()
         .forEach(
-            rating ->
-                System.out.printf(
-                    "%-4s %s %s%n",
-                    rating.getValue(),
-                    MovieDatabase.getYear(rating.getItem()),
-                    MovieDatabase.getTitle(rating.getItem())));
+            rating -> {
+              String movieID = rating.getItem();
+              System.out.printf("%-4s %s%n", rating.getValue(), MovieDatabase.getTitle(movieID));
+              System.out.println("     Year: " + MovieDatabase.getYear(movieID));
+              System.out.println("     Time: " + MovieDatabase.getMinutes(movieID));
+              System.out.println("     Genre(s): " + MovieDatabase.getGenres(movieID));
+              System.out.println("     Director(s): " + MovieDatabase.getDirector(movieID));
+            });
+    System.out.println("-------");
   }
 
   /**
@@ -65,12 +69,8 @@ public class MovieRunnerWithFilters {
    * @param year int Year of produce
    */
   public void printAverageRatingsByYear(int minimalRatings, int year) {
-    System.out.println("number of raters " + thirdRatings.getRaterSize());
-    System.out.println("number of movies " + MovieDatabase.size());
-    ArrayList<Rating> aveRating =
-        thirdRatings.getAverageRatingsByFilter(minimalRatings, new YearAfterFilter(year));
-    System.out.printf("found %d movies%n", aveRating.size());
-    printRatingsList(aveRating);
+    printRatingsList(
+        thirdRatings.getAverageRatingsByFilter(minimalRatings, new YearAfterFilter(year)));
   }
 
   /**
@@ -80,20 +80,8 @@ public class MovieRunnerWithFilters {
    * @param genre Genre
    */
   public void printAverageRatingsByGenre(int minimalRatings, String genre) {
-    ArrayList<Rating> averageRatings =
-        thirdRatings.getAverageRatingsByFilter(minimalRatings, new GenreFilter(genre));
-    System.out.printf("found %d movies%n", averageRatings.size());
-    averageRatings.stream()
-        .sorted()
-        .forEach(
-            rating -> {
-              String movieID = rating.getItem();
-              System.out.printf(
-                  "%-4s %s%n     %s%n",
-                  rating.getValue(),
-                  MovieDatabase.getTitle(movieID),
-                  MovieDatabase.getGenres(movieID));
-            });
+    printRatingsList(
+        thirdRatings.getAverageRatingsByFilter(minimalRatings, new GenreFilter(genre)));
   }
 
   /**
@@ -104,20 +92,19 @@ public class MovieRunnerWithFilters {
    * @param maxMinutes Maximum length of movies in minutes
    */
   public void printAverageRatingsByMinutes(int minimalRatings, int minMinutes, int maxMinutes) {
-    ArrayList<Rating> averageRatings =
+    printRatingsList(
         thirdRatings.getAverageRatingsByFilter(
-            minimalRatings, new MinutesFilter(minMinutes, maxMinutes));
-    System.out.printf("found %d movies%n", averageRatings.size());
-    averageRatings.stream()
-        .sorted()
-        .forEach(
-            rating -> {
-              String movieID = rating.getItem();
-              System.out.printf(
-                  "%-4s Time: %-3d %s%n",
-                  rating.getValue(),
-                  MovieDatabase.getMinutes(movieID),
-                  MovieDatabase.getTitle(movieID));
-            });
+            minimalRatings, new MinutesFilter(minMinutes, maxMinutes)));
+  }
+
+  /**
+   * Print a list of movies and their average ratings sorted by time
+   *
+   * @param minimalRatings Minimal number of ratings
+   * @param directors directors of the movies
+   */
+  public void printAverageRatingsByDirectors(int minimalRatings, String directors) {
+    printRatingsList(
+        thirdRatings.getAverageRatingsByFilter(minimalRatings, new DirectorsFilter(directors)));
   }
 }
